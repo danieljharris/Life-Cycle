99de37b6a8e3f2b0dc0b59f33b2c5220f88995e1
Daniel Harris <danieljharris26@yahoo.co.uk>
2026-02-02 23:38:47 +0000

    fix Ctrl+Clicl

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# install sdkman and java 21 (idempotent)
+SDKMAN_DIR="/usr/local/sdkman"
+if [ ! -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]; then
+    curl -s "https://get.sdkman.io" | bash
+fi
+source "$SDKMAN_DIR/bin/sdkman-init.sh"
+
+if [ ! -d "$SDKMAN_DIR/candidates/java/21.0.2-ms" ]; then
+    yes | sdk install java 21.0.2-ms
+fi
+sdk default java 21.0.2-ms
+
+# Install Gradle for IDE Ctrl+Click support (actual builds use Bazel)
+if [ ! -d "$SDKMAN_DIR/candidates/gradle/8.5" ]; then
+    yes | sdk install gradle 8.5
+fi
+sdk default gradle 8.5
+
 # install buildifier
 curl -L https://github.com/bazelbuild/buildtools/releases/download/v8.2.1/buildifier-linux-arm64 \
     -o /usr/local/bin/buildifier
